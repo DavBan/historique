d3dcf1a8566caac410b3c597dc93220cf86a3c06
David BANANIER <david.bananier@qorvo.com>
2025-03-12 10:10:25 +0100

    feat(testValidator): use create_tc api to create tcs.

--- testvalidators.py
+++ testvalidators.py
@@ -1,4 +1,5 @@
 from testing.testutils import Test, Tester
+from testing.testfactory import create_tc
 import datetime
 
 
@@ -21,9 +22,8 @@ class HandlerTest():
 
 
 
-class ValidateTest(Validator[HandlerTest], Test):
+class ValidateTest(Validator[HandlerTest]):
     def __init__(self, validate:bool = True):
-        Test.__init__(self, name="Validator_validate_"+str(validate), testedMethod=self.validate, parameters=[], expectedResults=validate)
         Validator.__init__(self, HandlerTest(validate))
 
     def validate(self):
@@ -39,5 +39,98 @@ class ValidatorTester(Tester):
 
         super().__init__("Class_Validator_Test", "This function tests the validator Class that provide a validate function", "./ValidatorTest_{}.log".format(datetime.datetime.today().strftime('%d-%m-%Y_%H-%M-%S')), listoftest=validator_tc)
 
-validator_test = ValidatorTester()
+validator_test = Tester(
+    "Class_Validator_Test",
+    "This function tests the validator Class that provide a validate function", 
+    "./ValidatorTest_{}.log".format(datetime.datetime.today().strftime('%d-%m-%Y_%H-%M-%S')), 
+    [
+        create_tc(
+            "Validator_validate_True",
+            ValidateTest.validate,
+            True,
+            cls=ValidateTest,
+            instance_init_param=[True]
+        ),
+        create_tc(
+            "Validator_validate_False",
+            ValidateTest.validate,
+            False,
+            cls=ValidateTest,
+            instance_init_param=[False]
+        )
+    ]
+)
+complexxvalidator_test = Tester(
+    "Class_Complexe_Validator_Test",
+    desc="This Test tests the ComplexValidator Class that provide a validate function",
+    logfile="./ComplexValidatorTest_{}.log".format(datetime.datetime.today().strftime('%d-%m-%Y_%H-%M-%S')),
+    listoftest=[
+        create_tc(
+            "ComplexeValidator_validate_false_only",
+            ComplexValidator.validate,
+            False,
+            [],
+            ComplexValidator,
+            [
+                [
+                    ValidateTest(False)
+                ]
+            ]
+        ),
+        create_tc(
+            "ComplexeValidator_validate_true_only",
+            ComplexValidator.validate,
+            True,
+            [],
+            ComplexValidator,
+            [
+                [
+                    ValidateTest(True)
+                ]
+            ]
+        ),
+        create_tc(
+            "ComplexeValidator_validate_two_Validator_false_first",
+            ComplexValidator.validate,
+            False,
+            [],
+            ComplexValidator,
+            [
+                [
+                    ValidateTest(False),
+                    ValidateTest(True)
+                ]
+            ]
+        ),
+        create_tc(
+            "ComplexeValidator_validate_two_Validator_false_second",
+            ComplexValidator.validate,
+            False,
+            [],
+            ComplexValidator,
+            [
+                [
+                    ValidateTest(True),
+                    ValidateTest(False)
+                ]
+            ]
+        ),
+        create_tc(
+            "ComplexeValidator_validate_two_Validator_true",
+            ComplexValidator.validate,
+            True,
+            [],
+            ComplexValidator,
+            [
+                [
+                    ValidateTest(True),
+                    ValidateTest(True)
+                ]
+            ]
+        )
+    ]
+)
+
+
 validator_test.run_all_tests()
+complexxvalidator_test.run_all_tests()
